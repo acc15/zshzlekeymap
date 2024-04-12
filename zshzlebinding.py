57f05dd942087ea5ea965ffcd7bc6bbb33bba9d1
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2024-04-12 04:17:02 +0300

    tables

--- zshzlebinding.py
+++ zshzlebinding.py
@@ -14,61 +14,19 @@ class ZshZleBinding:
     text: str
     type: ZshZleBindingType
 
-class CharIter:
-    value: str
-    pos: int
-    
-    def __init__(self, s: str, p: int = 0):
-        self.value = s
-        self.pos = p
+def q(name: str):
+    return fr'(?:"(?P<{name}>(?:\.|.)*?)")'
 
-    def has_next(self):
-        return self.pos < len(self.value)
+binding_pattern = re.compile(fr'{q("key")}(?:-{q("key_end")})? (?:{q("command")}|(?P<widget>.+))')
 
-    def peek(self):
-        return self.value[self.pos : self.pos + 1]
-    
-    def skip(self, amount: int = 1):
-        self.pos += amount
-
-    def next(self):
-        res = self.peek()
-        self.pos += 1
-        return res
-    
-    def remainder(self):
-        return self.value[self.pos:]
-
-def parse_quoted_string(it: CharIter) -> str:
-    assert it.next() == '"'
-    start_pos = it.pos
-    next_escape = False
-    while ch := it.next():
-        if next_escape:
-            next_escape = False
-            continue
-        if ch == '"':
-            break
-        if ch == '\\':
-            next_escape = True
-
-    return it.value[start_pos:it.pos-1]
-
-def parse_zshzle_binding(b: str) -> ZshZleBinding:
-    it = CharIter(b)
-    
-    key = parse_quoted_string(it)
-    key_end: Optional[str] = None
-    if it.next() == "-":
-        key_end = parse_quoted_string(it)
-        it.skip()
-    
+def parse_zshzle_binding(binding: str) -> ZshZleBinding:
+    if not (m := binding_pattern.fullmatch(binding)):
+        raise ValueError(f"can't parse zshzle binding: {binding}")
     text, type = (
-        (parse_quoted_string(it), ZshZleBindingType.COMMAND) 
-        if it.peek() == '"' else 
-        (it.remainder(), ZshZleBindingType.WIDGET) 
+        (m["widget"], ZshZleBindingType.WIDGET) if m["widget"] else 
+        (m["command"], ZshZleBindingType.COMMAND)
     )
-    return ZshZleBinding(key, key_end, text, type)
+    return ZshZleBinding(m["key"], m["key_end"], text, type)
 
 def get_zshzle_bindings():
     binding_strings = subprocess.run(
--- zshzlekey.py
+++ zshzlekey.py
@@ -1,39 +1,113 @@
-
-
 from dataclasses import dataclass
 from typing import Iterable, Optional
-from enum import Enum, auto
-from zshzlebinding import ZshZleBinding, ZshZleBindingType, get_zshzle_bindings
+from zshzlebinding import ZshZleBinding
 import re 
 
-keymap: dict[str, list[list[list[str]]]] = {
-    "^?": [
-        [
-            ["Backspace"]
-        ], 
-        [
-            ["Ctrl", "Shift", "/"]
-        ]
-    ],
-    "^M": [[["Enter"]], [["Ctrl", "M"]]],
-    "^I": [[["Tab"]], [["Ctrl", "I"]]]
+KeyChord = tuple[str, ...]
+
+@dataclass
+class EscapeChord:
+    escapes: list[str]
+    chords: list[list[str]]
+
+lookup_table = [
+    (["^M"], ["Enter"]),
+    (["^I"], ["Tab"]),
+    (["^?"], ["Backspace", ("Ctrl", "Shift", "/")]),
+    (["^H"], [("Ctrl", "Backspace")]),
+    
+    (["^[[2~"], ["Insert"]),
+    (["^[[2;3~"], [("Alt", "Insert")]),
+    (["^[[2;5~"], [("Ctrl", "Insert")]),
+    (["^[[2;7~"], [("Ctrl", "Alt", "Insert")]),
+    
+    (["^[[3~"], ["Delete"]),
+    (["^[[3;3~"], [("Alt", "Delete")]),
+    (["^[[3;4~"], [("Alt", "Shift", "Delete")]),
+    (["^[[3;5~"], [("Ctrl", "Delete")]),
+    (["^[[3;6~"], [("Ctrl", "Shift", "Delete")]),
+    (["^[[3;7~"], [("Ctrl", "Alt", "Delete")]),
+
+    (["^[[5~"], ["PgUp"]),
+    (["^[[5;3~"], [("Alt", "PgUp")]),
+    (["^[[5;5~"], [("Ctrl", "PgUp")]),
+    (["^[[5;6~"], [("Ctrl", "Shift", "PgUp")]),
+    (["^[[5;7~"], [("Ctrl", "Alt", "PgUp")]),
+
+    (["^[[6~"], ["PgDown"]),
+    (["^[[6;3~"], [("Alt", "PgDown")]),
+    (["^[[6;5~"], [("Ctrl", "PgDown")]),
+    (["^[[6;7~"], [("Ctrl", "Alt", "PgDown")]),
+
+    (["^[[H"], ["Home"]),
+    (["^[[1;3H"], [("Alt", "Home")]),
+    (["^[[1;5H"], [("Ctrl", "Home")]),
+    (["^[[1;7H"], [("Ctrl", "Alt", "Home")]),
+
+    (["^[[F"], ["End"]),
+    (["^[[1;3F"], [("Alt", "End")]),
+    (["^[[1;5F"], [("Ctrl", "End")]),
+    (["^[[1;7F"], [("Ctrl", "Alt", "End")]),
+
+    (["^[OA", "^[[A"], ["Up"]),
+    (["^[OB", "^[[B"], ["Down"]),
+    (["^[OC", "^[[C"], ["Right"]),
+    (["^[OD", "^[[D"], ["Left"]),
+]
+
+lookup_map = { 
+    key: frozenset([ chord if isinstance(chord, tuple) else (chord,) for chord in e[1] ]) 
+    for e in lookup_table 
+    for key in e[0] 
 }
 
-key_pattern = re.compile(r"(?P<alt>\^\[)?(?P<ctrl>\^)?(?P<char>.)")
+csi_sequence = r'(?P<csi>\^\[\[[\x30-\x3F]*[\x20-\x2F]*[\x40-\x7E])'
+char_sequence = r'(?P<esc>\^\[)?(?P<ctrl>\^)?)(?:\\\\\\(?P<slash>\\)|\\(?P<escchar>.)|(?P<char>.)'
+esc_sequence = fr"^(?:{csi_sequence}|{char_sequence})"
+esc_pattern = re.compile(esc_sequence)
 
-def parse_key(key: str) -> Optional[list[str]]:
+def get_chord_by_match(m: Optional[re.Match]) -> Optional[KeyChord]:
+    if m and not m["csi"]:
+        return tuple([k for k in [
+            "Ctrl" if m["ctrl"] else None,
+            "Alt" if m["esc"] else None,
+            next((m[g] for g in ["slash", "escchar", "char"] if m[g] )).upper()
+        ] if k])
 
-    m = key_pattern.fullmatch(key) 
-    if m:
-        result = []
-        if m.group("ctrl"):
-            result.append("Ctrl")
-        if m.group("alt"):
-            result.append("Alt")
-        result.append(m.group("char").upper())
-        return result
+def lookup_escape_map(seq: str) -> Optional[str]:
+    while seq:
+        if seq in lookup_map:
+            return seq
+        seq = seq[:-1]
 
-    return None
+def parse_escape_sequence(seq: str) -> list[set[KeyChord]]:
+    result = []
+    chords = set()
+    while seq:
+        lookup_seq = lookup_escape_map(seq)
+        if lookup_seq: # subsequence found in lookup table
+            
+            chords.update(lookup_map[lookup_seq])
+            lookup_chord = get_chord_by_match(esc_pattern.fullmatch(lookup_seq))
+            if lookup_chord:
+                chords.add(lookup_chord)
+            seq = seq[len(lookup_seq):]
+
+        else:
+            parsed_match = esc_pattern.search(seq)
+            parsed_chord = get_chord_by_match(parsed_match)
+            if parsed_chord:
+                chords.add(parsed_chord)
+                seq = seq[parsed_match.end():]
+
+        if not chords:
+            return None
+
+        if chords:
+            result.append(chords)
+            chords = set()
+
+    return result
 
 
 def kbd(key: str):
@@ -42,10 +116,11 @@ def kbd(key: str):
 def format_bindings(bindings: Iterable[ZshZleBinding]):
     return " | ".join([ kbd(b.key) for b in bindings ])
 
-print(parse_key("^[^N"))
-print(parse_key("^[^n"))
-print(parse_key("^[m"))
-print(parse_key("^Q"))
-print(parse_key("q"))
-print(parse_key("^Xa"))
-print(parse_key("aaa"))
+print( parse_escape_sequence("^[^n") )
+print( parse_escape_sequence("n") )
+print( parse_escape_sequence("\\\\\\\\") )
+print( parse_escape_sequence("^Q") )
+print( parse_escape_sequence("^M") )
+print( parse_escape_sequence("^[p") )
+print( parse_escape_sequence("^[^\\\\\\\\") )
+print( parse_escape_sequence("^[[3;5~^M^I") )
