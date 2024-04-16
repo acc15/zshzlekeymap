d7aeb8ff3b1f3c41bdfee56b091adc23492623fe
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2024-04-16 04:41:54 +0300

    renamed test

--- tests/test_zshzlekey.py
+++ tests/test_zshzlekey.py
@@ -15,7 +15,7 @@ class ZshZleKeyTests(unittest.TestCase):
                 r = zshzlekey.parse_keychord(esc, False)
                 self.assertEqual(r.chord, chord) if r is not None else self.assertIsNone(chord)
 
-    def get_actual_binding(self, code: int) -> str:
+    def get_bindkey_escape(self, code: int) -> str:
         km = "test"
         return parse_zshzle_binding(subprocess.run(
             ["zsh", "-c", f"bindkey -N {km} && bindkey -M {km} -s '\\x{code:0x}' '' && bindkey -M {km}"], 
@@ -26,14 +26,14 @@ class ZshZleKeyTests(unittest.TestCase):
 
     def test_parse_char_esc(self):
         for code in range(0x100):
-            esc = self.get_actual_binding(code)
+            esc = self.get_bindkey_escape(code)
             with self.subTest(esc = esc, code = code):
                 r = zshzlekey.parse_char_esc(esc)
                 self.assertEqual(code, r) if r is not None else self.assertIsNone(code)
 
     def test_format_char_esc(self):
         for code in range(0x100):
-            esc = self.get_actual_binding(code)
+            esc = self.get_bindkey_escape(code)
             with self.subTest(code = code, esc = esc):
                 r = zshzlekey.format_char_esc(code)
                 self.assertEqual(esc, r) if r is not None else self.assertIsNone(esc)
